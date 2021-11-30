c9388980a5a958c38b83ef89a8875395e8f6593a
LSZ <1817246943@qq.com>
2021-11-30 11:21:43 +0800

    修复字符串比较结果异常，但是内存分配还有问题，等待优化

--- Device_Manager/DM.cpp
+++ Device_Manager/DM.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include<string.h>
 #include <cstdlib>
+#include <cstdlib>
 #include <iomanip>
 
 using namespace std;
@@ -55,8 +56,8 @@ struct CHCT
 queue <PCB> ch_block;  /* 通道阻塞队列 */
 queue <PCB> co_block;  /* 控制器阻塞队列 */
 vector <PCB> run_p;    /* 申请成功的队列 */
-vector <SDT> sdt;   /* 系统设备表队列 */
-vector <COCT> coct;  /* 控制器表队列 */
+vector <SDT> sdt(20);   /* 系统设备表队列 */
+vector <COCT> coct(20);  /* 控制器表队列 */
 vector <CHCT> chct(2);  /* 通道控制表队列 */
 
 void show();    /* 显示设备信息 */
@@ -69,71 +70,58 @@ void Delete();  /* 删除设备 */
 /* 初始化 */
 void init(){
     /* 初始化SDT和DCT */
-    SDT sdt_init[4];
-    struct COCT coct_init[4];
-    CHCT chct_init[2];
 
-    sdt_init[0].Dname = "display1";
-    sdt_init[0].type = 'O';
-    sdt_init[0].DCT.Dname = sdt_init[0].Dname;
-    sdt_init[0].DCT.type = sdt_init[0].type;
-    sdt_init[0].DCT.state = 0;
-    sdt_init[0].DCT.coid = "co1";
-    sdt.push_back(sdt_init[0]);
-    sdt_init[1].Dname = "keyboard1";
-    sdt_init[1].type = 'I';
-    sdt_init[1].DCT.Dname = sdt_init[1].Dname;
-    sdt_init[1].DCT.type = sdt_init[1].type;
-    sdt_init[1].DCT.state = 0;
-    sdt_init[1].DCT.coid = "co2";
-    sdt.push_back(sdt_init[1]);
-    sdt_init[2].Dname = "mouse1";
-    sdt_init[2].type = 'I';
-    sdt_init[2].DCT.Dname = sdt_init[2].Dname;
-    sdt_init[2].DCT.type = sdt_init[2].type;
-    sdt_init[2].DCT.state = 0;
-    sdt_init[2].DCT.coid = "co3";
-    sdt.push_back(sdt_init[2]);
-    sdt_init[3].Dname = "printer1";
-    sdt_init[3].type = 'O';
-    sdt_init[3].DCT.Dname = sdt_init[3].Dname;
-    sdt_init[3].DCT.type = sdt_init[3].type;
-    sdt_init[3].DCT.state = 0;
-    sdt_init[3].DCT.coid = "co4";
-    sdt.push_back(sdt_init[3]);
+    sdt[0].Dname = "display1";
+    sdt[0].type = 'O';
+    sdt[0].DCT.Dname = sdt[0].Dname;
+    sdt[0].DCT.type = sdt[0].type;
+    sdt[0].DCT.state = 0;
+    sdt[0].DCT.coid = "co1";
+    sdt[1].Dname = "keyboard1";
+    sdt[1].type = 'I';
+    sdt[1].DCT.Dname = sdt[1].Dname;
+    sdt[1].DCT.type = sdt[1].type;
+    sdt[1].DCT.state = 0;
+    sdt[1].DCT.coid = "co2";
+    sdt[2].Dname = "mouse1";
+    sdt[2].type = 'I';
+    sdt[2].DCT.Dname = sdt[2].Dname;
+    sdt[2].DCT.type = sdt[2].type;
+    sdt[2].DCT.state = 0;
+    sdt[2].DCT.coid = "co3";
+    sdt[3].Dname = "printer1";
+    sdt[3].type = 'O';
+    sdt[3].DCT.Dname = sdt[3].Dname;
+    sdt[3].DCT.type = sdt[3].type;
+    sdt[3].DCT.state = 0;
+    sdt[3].DCT.coid = "co4";
     
 
     /* 初始化控制器控制表 */
-    // coct_init[0]("col",0,"ch1");
-    // strcpy((char*)&coct_init[0].controllerid,"col");
-    coct_init[0].controllerid = string("co1");
-    coct_init[0].state = 0;
-    coct_init[0].Chid = string("ch1");
-    coct.push_back(coct_init[0]);
-    coct_init[1].controllerid = string("co2");
-    coct_init[1].state = 0;
-    coct_init[1].Chid = string("ch1");
-    coct.push_back(coct_init[1]);
-    coct_init[2].controllerid = string("co3");
-    coct_init[2].state = 0;
-    coct_init[2].Chid = string("ch2");
-    coct.push_back(coct_init[2]);
-    coct_init[3].controllerid = string("co4");
-    coct_init[3].state = 0;
-    coct_init[3].Chid = string("ch2");
-    coct.push_back(coct_init[3]);
+    // coct[0]("col",0,"ch1");
+    // strcpy((char*)&coct[0].controllerid,"col");
+    coct[0].controllerid = string("co1");
+    coct[0].state = 0;
+    coct[0].Chid = string("ch1");
+    coct[1].controllerid = string("co2");
+    coct[1].state = 0;
+    coct[1].Chid = string("ch1");
+    coct[2].controllerid = string("co3");
+    coct[2].state = 0;
+    coct[2].Chid = string("ch2");
+    coct[3].controllerid = string("co4");
+    coct[3].state = 0;
+    coct[3].Chid = string("ch2");
 
     /* 初始化通道控制表 */
-    chct_init[0].channelid = "ch1";
-    chct_init[0].state = 0;
-    chct_init[0].COid.push_back("co1");
-    chct_init[0].COid.push_back("co2");
-    chct.push_back(chct_init[0]);
-    chct_init[1].channelid = "ch2";
-    chct_init[1].state = 0;
-    chct_init[1].COid.push_back("co3");
-    chct_init[1].COid.push_back("co4");
-    chct.push_back(chct_init[1]);
+    chct[0].channelid = string("ch1");
+    chct[0].state = 0;
+    chct[0].COid.push_back("co1");
+    chct[0].COid.push_back("co2");
+    chct[1].channelid = string("ch2");
+    chct[1].state = 0;
+    chct[1].COid.push_back("co3");
+    chct[1].COid.push_back("co4");
 }
 
 /* 添加设备 */
@@ -184,7 +172,7 @@ void Add(){
             std::cout<<"请输入添加控制器的id：";
             std::cin>>newco.controllerid;
             for(int k=0; k<coct.size(); k++){
-                if(newco.controllerid.compare(coct[k].controllerid) == 0){
+                if(newco.controllerid == coct[k].controllerid){
                     k = 0;
                     std::cout<<"控制器已存在,请重新添加"<<endl<<endl;
                     std::cin>>newco.controllerid;
@@ -193,12 +181,12 @@ void Add(){
             newDevice.DCT.coid = newco.controllerid;  /* 将新设备与添加的控制器连接起来 */
             std::cout<<"请输入控制器所连通道的id(ch1,ch2)：";
             std::cin>>newco.Chid;
-            while(newco.Chid.compare("ch1") != 0 && newco.Chid.compare("ch2") != 0){
+            while(newco.Chid != "ch1" && newco.Chid !="ch2"){
                 std::cout<<"输入不合法，请重新输入!!!"<<endl;
                 std::cin>>newco.Chid;
             }
             /* 在通道控制表中加入新添加的控制器 */
-            if(newco.Chid.compare("ch1") == 0)  /* 说明新添加的控制器与通道1相连 */
+            if(newco.Chid == "ch1")  /* 说明新添加的控制器与通道1相连 */
                 chct[0].COid.push_back(newco.controllerid);
             else  /* 说明新添加的控制器与通道2相连 */
                 chct[1].COid.push_back(newco.controllerid);
@@ -231,17 +219,18 @@ void Distribute(){
             else if(sdt[i].DCT.state == 0){  /* 设备空闲 */
                 sdt[i].DCT.state = 1;
                 for(int k=0; k<coct.size(); k++){  /* 查找控制该设备的控制器 */
-                    if(sdt[i].DCT.coid.compare(coct[k].controllerid) == 0){
+                    if(sdt[i].DCT.coid == coct[k].controllerid){
                         if(coct[k].state == 0){  /* 控制器空闲 */
                             process.coid = coct[k].controllerid;    /* 将空闲的控制器交给申请进程使用 */
                             coct[k].state = 1;
-                            if(coct[k].Chid.compare("ch1") && chct[0].state == 0){  /* 表明控制器与通道1相连且通道1空闲 */
+                            if(coct[k].Chid == "ch1" && chct[0].state == 0){  /* 表明控制器与通道1相连且通道1空闲 */
                                 process.chid = "ch1";   /* 该进程占用通道1 */
                                 chct[0].state = 1;  /* 将通道1状态置1(忙碌) */
+                                run_p.push_back(process);
                                 std::cout<<"分配设备成功"<<endl;
                                 return;
                             }
-                            else if (coct[k].Chid.compare("ch2") && chct[1].state == 0){   /* 表明控制器与通道2相连且通道2空闲 */
+                            else if (coct[k].Chid == "ch2" && chct[1].state == 0){   /* 表明控制器与通道2相连且通道2空闲 */
                                 process.chid = "ch2";   /* 该进程占用通道2 */
                                 chct[1].state = 1;   /* 将通道2状态置1(忙碌) */
                                 run_p.push_back(process);
@@ -276,20 +265,20 @@ void Recycle(){
     std::cout<<"请输入回收设备id：";
     std::cin>>Deviceid;
     for(int i=0; i<sdt.size(); i++){
-        if(sdt[i].Dname.compare(Deviceid) == 0){    /* 寻找待回收设备 */
+        if(sdt[i].Dname == Deviceid){    /* 寻找待回收设备 */
             if(sdt[i].DCT.state == 1){  /* 如果待回收设备正在使用中 */
                 sdt[i].DCT.state = 0;   /* 设备状态置为空闲 */
                 for(int k=0; k<run_p.size(); k++)
-                    if(run_p[k].device_name.compare(Deviceid) == 0){    /* 查找使用该设备的进程 */
-                        controlid = run_p[k].coid;   /* 记录进程占用的控制器 */
-                        channelid = run_p[k].chid;   /* 记录进程占用通道 */
+                    if(run_p[k].device_name == Deviceid){    /* 查找使用该设备的进程 */
+                        controlid.assign(run_p[k].coid);   /* 记录进程占用的控制器 */
+                        channelid.assign(run_p[k].chid);   /* 记录进程占用通道 */
                         run_p.erase(run_p.begin()+k);   /* 释放该进程 */
                     }
                 for(int j=0; j<coct.size(); j++)   /* 查找设备占用的控制器 */
-                    if(coct[j].controllerid.compare(controlid) == 0)
+                    if(coct[j].controllerid == controlid)
                         coct[j].state = 0;  /* 找到后将控制器状态置为空闲 */
                 for(int channel=0; channel<chct.size(); channel++)  /* 查找设备占用通道 */
-                    if(chct[channel].channelid.compare(channelid) == 0)
+                    if(chct[channel].channelid == channelid)
                         chct[channel].state = 0;    /* 找到后将通道状态置为空闲 */
                 std::cout<<"设备成功回收"<<endl<<endl;
                 return;
@@ -312,7 +301,7 @@ void Delete(){
     std::cout<<"请输入删除设备的名称:";
     std::cin>>Device;
     for(int i=0; i<sdt.size(); i++){    /* 定位将要删除的设备 */
-        if(sdt[i].Dname.compare(Device) == 0){  /* 找到要删除的设备 */
+        if(sdt[i].Dname == Device){  /* 找到要删除的设备 */
             dflag:if(sdt[i].DCT.state == 0){  /* 检查状态，设备未被占用 */
                     std::cout<<"是否删除设备"<<sdt[i].Dname<<"[y/n]:";
                     std::cin>>confirm;
@@ -335,15 +324,15 @@ void Delete(){
             else{   /* 如果设备正在使用中 */
                 sdt[i].DCT.state = 0;
                 for(int k=0; k<run_p.size(); k++)  /* 首先找到使用该设备的进程 */
-                    if(run_p[k].device_name.compare(Device) == 0){
+                    if(run_p[k].device_name == Device){
                         controller = run_p[k].coid;  /* 记录控制器id */
                         channel = run_p[k].chid;    /* 记录通道id */
                         run_p.erase(run_p.begin()+k);   /* 释放该进程，即删除 */
                     }
                 for(int j=0; j<coct.size(); j++)    /* 定位设备所使用控制器的位置 */
-                    if(coct[j].controllerid.compare(controller) == 0)   /* 找到该控制器，置0 */
+                    if(coct[j].controllerid == controller)   /* 找到该控制器，置0 */
                         coct[j].state = 0;
-                if(channel.compare("ch1") == 0) /* 这两个判断语句寻找设备使用通道，找到置0 */
+                if(channel == "ch1") /* 这两个判断语句寻找设备使用通道，找到置0 */
                     chct[0].state = 0;
                 else
                     chct[1].state = 0;
